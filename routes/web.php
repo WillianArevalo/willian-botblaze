049ca0b068ddb4ff944941db2fa836beaae39fd0
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-06 13:33:36 -0600

    feat: add routes for login, dashboard and products

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::controller(LoginController::class)->group(function () {
+    Route::get("/", "index")->name("login");
+    Route::post("/validate", "validate")->name("login.validate");
+    Route::post("/logout", "logout")->name("logout");
 });
+
+Route::middleware(["auth", "role:admin"])->group(function () {
+    Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
+});
+
+Route::resource("products", ProductController::class)->middleware(["auth", "role:admin"]);
