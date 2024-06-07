771a5ee7091ad6d3ef235c8084cb2962c7b40034
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-07 16:02:01 -0600

    chore: add links in the head

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -12,9 +12,15 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 </head>
 
-<body class="overflow-hidden bg-black">
+<body class="relative overflow-x-hidden bg-black body">
+
+    @include('layouts.__partials.alert')
+
     @if (Route::currentRouteName() !== 'login')
         @include('layouts.__partials.navbar')
     @endif
