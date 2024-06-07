0de7317c7fe3afa816c1a83a1a50d3d63a315904
Willian Ernesto Arevalo Rodriguez <willian.arevalo22@itca.edu.sv>
2024-06-07 14:17:03 -0600

    refactor: add routes sources for movements

--- routes/web.php
+++ routes/web.php
@@ -17,15 +17,9 @@ Route::middleware(["auth", "role:admin"])->group(function () {
     Route::get("/dashboard", [DashboardController::class, "index"])->name("dashboard");
 });
 
+Route::post("/products/search", [ProductController::class, "search"])->name("products.search");
 Route::resource("products", ProductController::class)->middleware(["auth", "role:admin"]);
 
-Route::middleware(["auth", "role:admin"])->group(function () {
-    Route::get("/movements", [MovementController::class, "index"])->name("movements.index");
-    Route::get("/movements/output", [MovementController::class, "output"])->name("movements.output");
-    Route::get("/movements/input", [MovementController::class, "input"])->name("movements.input");
-    Route::post("/movements/store", [MovementController::class, "store"])->name("movements.store");
-    Route::get("/movements/{id}", [MovementController::class, "show"])->name("movements.show");
-    Route::get("/movements/{id}/edit", [MovementController::class, "edit"])->name("movements.edit");
-    Route::put("/movements/{id}", [MovementController::class, "update"])->name("movements.update");
-    Route::delete("/movements/{id}", [MovementController::class, "destroy"])->name("movements.destroy");
-});
+Route::get("/movements/output", [MovementController::class, "output"])->name("movements.output");
+Route::get("/movements/input", [MovementController::class, "input"])->name("movements.input");
+Route::resource("movements", MovementController::class)->middleware(["auth", "role:admin"]);
